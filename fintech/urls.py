871e1b677db79d4c80e004364b36320fe4ac3849
arhamfareed106 <arhamfareed575@gmail.com>
2025-07-20 02:06:37 +0500

    Enhance login.html template with improved structure, styling, and password visibility toggle

--- fintech/urls.py
+++ fintech/urls.py
@@ -4,4 +4,5 @@ from fintech import views  # Import views from the fintech app
 
 urlpatterns = [
     path('register/', views.RegisterView.as_view(), name='register'),
+    # path('login/', views.LoginView.as_view(), name='login'),
 ]
